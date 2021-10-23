dbbfab72b67029c213d5f6dc7ca2ddd9626d76bd
Ingo Buescher <gallatin@gmx.net>
2021-10-23 12:14:01 +0200

    fix tests

--- tests/test_umwerter.rs
+++ tests/test_umwerter.rs
@@ -3,23 +3,23 @@ use umwerter::errors;
 
 #[test]
 fn umwerter_18265_a1() {
-    let mut erg = umwerter::werte_um(260f64, "MPa", "HV 10", &konstanten::UmwertungsTabelle::Iso18265A1);
+    let mut erg = umwerter::werte_um(260f64, "MPa", "HV 10", konstanten::UmwertungsTabelle::Iso18265A1);
     assert!(erg.is_ok());
     assert_eq!(erg.unwrap(), 81.66666666666667);
 
-    erg = umwerter::werte_um(260f64, "MPa", "HRF", &konstanten::UmwertungsTabelle::Iso18265A1);
+    erg = umwerter::werte_um(260f64, "MPa", "HRF", konstanten::UmwertungsTabelle::Iso18265A1);
     assert!(erg.is_err());
     assert!(match erg.unwrap_err() { errors::UmwerterError::ZielWertAusserhalbUmwertungsnorm(_) => true, _ => false });
 
-    erg = umwerter::werte_um(200f64, "MPa", "HV 10", &konstanten::UmwertungsTabelle::Iso18265A1);
+    erg = umwerter::werte_um(200f64, "MPa", "HV 10", konstanten::UmwertungsTabelle::Iso18265A1);
     assert!(erg.is_err());
     assert!(match erg.unwrap_err() { errors::UmwerterError::QuellWertAusserhalbUmwertungsnorm(_) => true, _ => false });
 
-    erg = umwerter::werte_um(200f64, "XXX", "HV 10", &konstanten::UmwertungsTabelle::Iso18265A1);
+    erg = umwerter::werte_um(200f64, "XXX", "HV 10", konstanten::UmwertungsTabelle::Iso18265A1);
     assert!(erg.is_err());
     assert!(match erg.unwrap_err() { errors::UmwerterError::QuellEinheitNichtVorhanden(_) => true, _ => false });
 
-    erg = umwerter::werte_um(200f64, "MPa", "XXX", &konstanten::UmwertungsTabelle::Iso18265A1);
+    erg = umwerter::werte_um(200f64, "MPa", "XXX", konstanten::UmwertungsTabelle::Iso18265A1);
     assert!(erg.is_err());
     assert!(match erg.unwrap_err() { errors::UmwerterError::ZielEinheitNichtVorhanden(_) => true, _ => false });
 
@@ -27,7 +27,7 @@ fn umwerter_18265_a1() {
 
 #[test]
 fn umwerter_18265_b2() {
-    let erg = umwerter::werte_um(215f64, "HV", "HBW", &konstanten::UmwertungsTabelle::Iso18265B2);
+    let erg = umwerter::werte_um(215f64, "HV", "HBW", konstanten::UmwertungsTabelle::Iso18265B2);
     assert!(erg.is_ok());
     assert_eq!(erg.unwrap(), 210f64);
 }
